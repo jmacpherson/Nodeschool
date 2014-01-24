6e68c314fd504276408c88c665793ba9e0121509
jmacpherson <jd.macpherson@gmail.com>
2014-01-24 14:51:46 -0500

    completed html stream exercise

--- stream-adventure/htmlStream.js
+++ stream-adventure/htmlStream.js
@@ -1,1 +1,14 @@
-htmlStream.js
+var trumpet = require('trumpet');
+var through = require('through');
+var tr = trumpet();
+process.stdin.pipe(tr).pipe(process.stdout);
+
+var stream = tr.createStream('.loud');
+stream.pipe(through(write, end)).pipe(stream);
+
+function write(buf){
+  this.queue(buf.toString().toUpperCase());
+}
+function end(){
+  this.queue(null);
+}
